4e3487891f30b300559ebd3e40ac62dd757036dd
Viacheslav Olishevko <slavaolish@gmail.com>
2021-02-17 18:24:36 +0300

    Divide client and server build configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,29 @@
 const path = require('path');
 const webpackNodeExternals = require('webpack-node-externals');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = [
     {
         mode: 'development',
-        entry: './app/app.ts',
-        target: 'node',
-        resolve: {
-            extensions: ['.ts', '.js'],
-        },
-        output: {
-          path: path.resolve(__dirname, 'dist/app'),
-          filename: 'app.bundle.js',
-        },
-        module: {
-            rules: [
-                {
-                    test:/\.ts$/,
-                    use: [
-                        'ts-loader',
-                    ],
-                },
-            ],
-        },
-        externals: [ webpackNodeExternals() ]
-    },
-    {
-        mode: 'development',
         entry: './public/clientapp.ts',
+        plugins:[
+            new HTMLWebpackPlugin(),
+        ],
         resolve: {
             extensions: ['.ts', '.js'],
         },
         output: {
             path: path.resolve(__dirname, 'dist/public'),
-            filename: 'clientapp.js',
+            filename: 'clientapp.bundle.js',
         },
         module: {
             rules: [
                 {
                     test: /\.ts$/,
-                    use: [
-                        'ts-loader',
-                    ],
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: 'tsconfig.webpack.json',
+                    },
                 },
             ],
         },
